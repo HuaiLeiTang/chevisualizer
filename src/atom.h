8f0ef6bd80f25ab90001450ed2c2995b6e36900d
Shreya Agarwal <friendforallshreya@gmail.com>
2016-01-22 13:28:00 +0545

    Created Atom Object prototype

--- src/atom.h
+++ src/atom.h
@@ -4,7 +4,21 @@
 class Atom
 {
 public:
+    Atom(); //< constructor
+    Atom(char symbol[], float scale); //< creates the atom
+    ~Atom(); //< destructor
+    bool setSymbol(char symbol[]);//< sets atom symbol
+    bool setScale(float scale);//< sets the scale
+    bool setColor(char color[]); //< sets colorname
+    bool setColor(int color); //<sets hex color
 private:
+    bool calRadiusPixel(float radius); // calculates radius pixel
+    char symbol[3]; //< chemical symbol of atom
+    static float scale; //< scale
+    char color[10]; //< color name
+    int hexcolor; //< color in hex
+    float radius; //< radius of the atom
+    int radiusPixel; //< radius in Pixel
 };
 
 #endif // ATOM_H
